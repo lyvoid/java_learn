cd5faad2ae6fbdf1a5a34ea8ef70fe91ccbaee5f
yang <735600970@qq.com>
2017-03-30 09:03:04 +0800

    quick sort & major element select

--- DataStructLearn/src/yang/Sort.java
+++ DataStructLearn/src/yang/Sort.java
@@ -1,9 +1,16 @@
 package yang;
 
+import java.util.Random;
+
 public class Sort {
     public static void main(String[] args) {
-        int[] test = new int[]{1,2,2,2,4,4,4,5,3,3,3,3};
-        bucketSort(test);
+        int[] test = new int[] { 4, 4, 4, 4, 5, 3, 3 };
+        // quickSort(test, 0, test.length);
+        // for (int i : test) {
+        // System.out.println(i);
+        // }
+        System.out.println(majEleCandidateByiter(test));
+        System.out.println(selectMajEle(test));
     }
 
     static public void bucketSort(int[] nums) {
@@ -12,10 +19,10 @@ public class Sort {
         int[] result = new int[nums.length];
         int[] counts = new int[max - min + 1];
         for (int i : nums) {
-            counts[i - min] ++;
+            counts[i - min]++;
         }
-        for (int i=1; i<counts.length;i++){
-            counts[i] += counts[i-1];
+        for (int i = 1; i < counts.length; i++) {
+            counts[i] += counts[i - 1];
         }
         for (int i : nums) {
             result[--counts[i]] = i;
@@ -24,4 +31,69 @@ public class Sort {
             System.out.println(i);
         }
     }
+
+    public static void quickSort(int[] nums, int lo, int hi) {
+        if (hi - 1 <= lo)
+            return;
+        Random rd = new Random();
+        swap(nums, lo, lo + rd.nextInt(hi - lo));
+        int pivot = nums[lo];
+        int mi = lo + 1;
+        for (int i = mi; i < hi; i++) {
+            if (nums[i] < pivot)
+                swap(nums, mi++, i);
+        }
+        swap(nums, lo, mi - 1);
+        quickSort(nums, lo, mi);
+        quickSort(nums, mi, hi);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public static int selectMajEle(int[] nums) {
+        int count = 0;
+        int candidate = majEleCandidateByRecusion(nums, 0, nums.length);
+        for (int i : nums) {
+            if (i == candidate)
+                count++;
+        }
+        if (count >= nums.length / 2)
+            return candidate;
+        return -1;
+    }
+
+    private static int majEleCandidateByRecusion(int[] nums, int lo, int hi) {
+        if (lo == hi - 1) {
+            return nums[lo];
+        }
+        int count = 0;
+        for (int i = lo; i < hi; i++) {
+            if (nums[i] == nums[lo]) {
+                count++;
+            }
+            if (count == (i - lo + 1) / 2) {
+                return majEleCandidateByRecusion(nums, i, hi);
+            }
+        }
+        return nums[lo];
+    }
+
+    private static int majEleCandidateByiter(int[] nums) {
+        int lo = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[lo] == nums[i])
+                count++;
+            if (count == (i - lo + 1) / 2) {
+                lo = i;
+                count = 0;
+            }
+        }
+        return nums[lo];
+    }
+
 }
