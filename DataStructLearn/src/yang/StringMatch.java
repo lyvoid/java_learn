161bf425151f5426efaaa667f3e727873e1e931a
yang <735600970@qq.com>
2017-03-31 19:09:00 +0800

    KMP

--- DataStructLearn/src/yang/StringMatch.java
+++ DataStructLearn/src/yang/StringMatch.java
@@ -3,21 +3,40 @@ package yang;
 public class StringMatch {
 
     public static void main(String[] args) {
-        String s = "123456789p43r5ui03q42b9485jmj0cvf294u5rmweoaufjioaweps;uitrbm03vq498wi54u09dxqiirpjkwfdosa hjt0b oei";
-        String t = "s;uitrbm03vq43-8w";
+        String s = "000000000001";
+        String t = "00001";
         System.out.println(bruteMatch(t, s, 0, s.length()));
+        System.out.println(KMPMatch(t, s, 0, s.length()));
     }
 
     public static int KMPMatch(String t, String s, int start, int end) {
-        return 0;
+        int[] table = KMPTable(t);
+        for (int i = start, j = 0; i < end - start;) {
+            if (j < 0 || t.charAt(j) == s.charAt(i)) {
+                i++;
+                j++;
+            } else {
+                j = table[j];
+            }
+            if (j == t.length())
+                return i - t.length();
+        }
+        return -1;
     }
-    
-    private static int[] KMPTable(String t){
-        return null;
+
+    private static int[] KMPTable(String t) {
+        int[] table = new int[t.length()];
+        table[0] = -1;
+        for (int i = 1; i < t.length(); i++) {
+            if (t.charAt(i) == t.charAt(table[i-1] + 1)){
+                table[i] = 
+            }
+        }
+        return table;
     }
 
     public static int bruteMatch(String t, String s, int start, int end) {
-        for (int i = start; i < end - t.length(); i++) {
+        for (int i = start; i < end - t.length() + 1; i++) {
             for (int j = 0; j < t.length(); j++) {
                 if (t.charAt(j) != s.charAt(i + j))
                     break;
